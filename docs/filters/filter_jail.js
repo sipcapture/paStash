01900a51d8b0169d1fe7e55bc73aa5844b1a1398
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2017-09-19 20:02:34 +0200

    Create filter_jail.js
    Filter "jails" the returned string or data object into a new object's key, returned to the pipeline

---
+++ docs/filters/filter_jail.js
@@ -0,0 +1,28 @@
+var base_filter = require('../lib/base_filter'),
+  util = require('util'),
+  logger = require('log4node');
+
+function FilterJail() {
+  base_filter.BaseFilter.call(this);
+  this.mergeConfig({
+    name: 'Jail',
+    optional_params: ['target_field'],
+    default_values: { target_field: 'data' },
+    start_hook: this.start,
+  });
+}
+
+util.inherits(FilterJail, base_filter.BaseFilter);
+
+FilterJail.prototype.start = function(callback) {
+  logger.info('Initializing Jail filter with target:', this.target_field);
+  callback();
+};
+
+FilterJail.prototype.process = function(data) {
+  this.emit('data', { [this.target_field]: data.message || data });
+};
+
+exports.create = function() {
+  return new FilterJail();
+};
