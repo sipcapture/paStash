08f163dc9035a7ce545b753cc0fcb74db565f889
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2020-12-17 00:37:32 +0100

    update hyper plugins

--- plugins/inputs/hyperbeam/input_hyperbeam.js
+++ plugins/inputs/hyperbeam/input_hyperbeam.js
@@ -6,12 +6,9 @@ const Hyperbeam = require('hyperbeam')
 
 function InputHyperb() {
   base_input.BaseInput.call(this);
-  this.mergeConfig(ssl_helper.config());
   this.mergeConfig(this.unserializer_config());
   this.mergeConfig({
     name: 'HyperBeam',
-    host_field: 'host',
-    port_field: 'port',
     optional_params: ['path'],
     default_values: {
       'path': false,
@@ -24,42 +21,41 @@ util.inherits(InputHyperb, base_input.BaseInput);
 
 InputHyperb.prototype.start = function(callback) {
   logger.info('Start listening on hyperbeam', this.path);
-  if (!this.path) {
-	console.error('No Beam Key!');
-	return;
-  }
-
+  if (!this.path) { logger.info('No Beam Key!'); return; }
+  try {
   this.beam = new Hyperbeam(this.path)
-
   this.beam.on('remote-address', function ({ host, port }) {
-    if (!host) console.error('[hyperbeam] Could not detect remote address')
-    else console.error('[hyperbeam] Joined the DHT - remote address is ' + host + ':' + port)
-    if (port) console.error('[hyperbeam] Network is holepunchable \\o/')
+    if (!host) logger.info('[hyperbeam] Could not detect remote address')
+    else logger.info('[hyperbeam] Joined the DHT - remote address is ' + host + ':' + port)
+    if (port) logger.info('[hyperbeam] Network is holepunchable \\o/')
+    callback()
   })
 
   this.beam.on('connected', function () {
-    console.error('[hyperbeam] Success! Encrypted tunnel established to remote peer')
+    logger.info('[hyperbeam] Success! Encrypted tunnel established to remote peer')
   })
 
-  this.beam.on('end', () => beam.end())
+  // this.beam.on('end', () => this.beam.end())
+  this.beam.on('end', function(){
+	logger.info('stream ended');
+  	this.beam.destroy();
+	this.close_modules(process.exit(0));
+  }.bind(this))
 
-  this.beam.on('data', function(data) {
-	this.emit(data);
+  this.beam.on('data', function(chunk) {
+    this.emit( 'data', { message: chunk.toString(), source: 'hyper' } );
   }.bind(this));
 
   this.beam.on('error', function(err) {
     this.emit('error', err);
-  }.bind(this));
+  }.bind(this))
 
+  } catch(e) { logger.error(e); }
 };
 
 InputHyperb.prototype.close = function(callback) {
-  logger.info('Closing hyperbeam', this.path);
-  beam.destroy()
-  beam.on('close', function () {
-    callback();
-  })
-
+  logger.info('Closing hyperbeam input', this.path);
+  callback();
 };
 
 exports.create = function() {
--- plugins/outputs/hyperbeam/output_hyperbeam.js
+++ plugins/outputs/hyperbeam/output_hyperbeam.js
@@ -5,58 +5,65 @@ var base_output = require('@pastash/pastash').base_output,
 const Hyperbeam = require('hyperbeam');
 
 function OutputHyperb() {
+  base_output.BaseOutput.call(this);
   this.mergeConfig(this.serializer_config('raw'));
-  this.mergeConfig(error_buffer.config(function() {
-    return 'output hyperbeam to ' + this.path;
-  }));
   this.mergeConfig({
     name: 'HyperBeam',
-    optional_params: ['path'],
+    optional_params: [ 'path' ],
     default_values: {
-      'path': false,
+      'path': false
     },
+    start_hook: this.start
   });
 }
 
 util.inherits(OutputHyperb, base_output.BaseOutput);
 
-OutputHyperb.prototype.process = function(data) {
-  this.beam.push(data);
-};
-
 OutputHyperb.prototype.start = function(callback) {
   logger.info('Start listening on hyperbeam', this.path);
   if (!this.path) {
-        console.error('No Beam Key!');
+        logger.error('No Beam Key!');
         return;
   }
-  this.beam = new Hyperbeam(this.path)
+  this.beam = new Hyperbeam(this.path.toString());
 
   this.beam.on('remote-address', function ({ host, port }) {
-    if (!host) console.error('[hyperbeam] Could not detect remote address')
-    else console.error('[hyperbeam] Joined the DHT - remote address is ' + host + ':' + port)
-    if (port) console.error('[hyperbeam] Network is holepunchable \\o/')
+    if (!host) logger.info('[hyperbeam] Could not detect remote address')
+    else logger.info('[hyperbeam] Joined the DHT - remote address is ' + host + ':' + port)
+    if (port) logger.info('[hyperbeam] Network is holepunchable \\o/')
+    callback();
   })
 
   this.beam.on('connected', function () {
-    console.error('[hyperbeam] Success! Encrypted tunnel established to remote peer')
+    logger.info('[hyperbeam] Success! Encrypted tunnel established to remote peer')
   })
 
-  this.beam.on('end', () => beam.end())
+  // this.beam.on('end', () => this.beam.end())
+  this.beam.on('data', function(data){
+	logger.info('got',data);
+  })
 
   this.beam.on('error', function(err) {
-    this.emit('error', err);
-  }.bind(this));
+    logger.error('error', err);
+  })
 
-  callback();
 };
+
+OutputHyperb.prototype.process = function(data) {
+  if (this.beam) {
+	this.beam.write(JSON.stringify(data));
+  }
+};
+
 
 OutputHyperb.prototype.close = function(callback) {
   logger.info('Closing hyperbeam output to', this.path);
-  beam.destroy()
-  beam.on('close', function () {
-    callback();
-  })
+  if (this.beam) {
+	this.beam.destroy()
+	this.beam.on('close', function () {
+		callback();
+  	})
+  } else { callback(); }
 };
 
 exports.create = function() {
