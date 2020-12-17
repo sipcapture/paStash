b7288a1da7174f188a43ca8036210aebbcb00d9d
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2020-12-17 01:02:21 +0100

    add hyperswarm plugs

--- plugins/outputs/hyperswarm/node_modules/xor-distance/example.js
+++
@@ -1,6 +0,0 @@
-var distance = require('./')
-
-var dist1 = distance(new Buffer('foo'), new Buffer('bar'))
-var dist2 = distance(new Buffer('foo'), new Buffer('baz'))
-
-console.log(distance.gt(dist1, dist2))
