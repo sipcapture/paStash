8a8b50199658c37c785c9ba7bd928df08c6ecee2
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2021-10-02 17:46:10 +0200

    resync matrix plugins

--- plugins/inputs/matrix/input_matrix.js
+++ plugins/inputs/matrix/input_matrix.js
@@ -2,20 +2,19 @@ var base_input = require('@pastash/pastash').base_input,
   util = require('util'),
   logger = require('@pastash/pastash').logger;
 
-global.Olm = require('olm');
-var sdk = require("matrix-js-sdk");
-
 function InputMatrix() {
   base_input.BaseInput.call(this);
   this.mergeConfig(this.unserializer_config());
   this.mergeConfig({
     name: 'Matrix',
-    optional_params: ['path', 'userid', 'token', 'roomid', 'server'],
+    optional_params: ['userId', 'token', 'roomId', 'server', 'olm', 'debug'],
     default_values: {
       'server': 'https://matrix.org',
       'userId': false,
       'roomId': false,
-      'token': false
+      'token': false,
+      'olm': false,
+      'debug': false
     },
     start_hook: this.start,
   });
@@ -24,37 +23,42 @@ function InputMatrix() {
 util.inherits(InputMatrix, base_input.BaseInput);
 
 InputMatrix.prototype.start = function(callback) {
-  logger.info('Start listening on hyperbeam', this.path);
+  logger.info('Start listening on matrix', this.roomId);
+
+  if (this.olm) global.Olm = require('olm');
+  var sdk = require("matrix-js-sdk");
+
   if (!this.token||!this.userId||!this.roomId) { logger.info('Missing Settings!'); return; }
   try {
-	this.matrixClient = sdk.createClient({
+	var matrixClient = sdk.createClient({
 	    baseUrl: this.server,
 	    accessToken: this.token,
 	    userId: this.userId
-	}.bind(this));
-	matrixClient.initCrypto();
-	var viewingRoom = false;
+	});
+	if (this.olm) this.matrixClient.initCrypto();
+	this.viewingRoom = false;
 	matrixClient.joinRoom(this.roomId).then(function(room) {
                     this.viewingRoom = room;
 		    callback();
-                }, function(err) {
+
+                }.bind(this), function(err) {
                     logger.error("Matrix Join Error: %s", err);
-                }.bind(this));
+		    return;
+                });
 
-	this.matrixClient.on("sync", function(state, prevState, data) {
-	    logger.info("Matrix Sync", state);
-	});
-
-	this.matrixClient.on("Room", function(state) {
-	    logger.info("Matrix Sync", state);
-	});
-
-	this.matrixClient.on("Room.timeline", function(event, room, toStartOfTimeline) {
-	    if (toStartOfTimeline) {
-	        return; // don't emit paginated results
-	    }
-	    this.emit('data', { message: event.event, source: 'matrix' });
+	matrixClient.on("sync", function(state, prevState, data) {
+	        if (this.debug) logger.info("Matrix Sync", state);
 	}.bind(this));
+
+	matrixClient.on("Room", function(state) {
+	        if (this.debug) logger.info("Matrix Sync", state);
+	}.bind(this));
+
+	matrixClient.on("Room.timeline", function(event, room, toStartOfTimeline) {
+	        this.emit('data', { message: event.event, source: 'matrix' });
+	}.bind(this));
+
+	matrixClient.startClient(0);
 
   } catch(e) { logger.error(e); }
 };
--- plugins/outputs/matrix/output_matrix.js
+++ plugins/outputs/matrix/output_matrix.js
@@ -2,20 +2,18 @@ var base_output = require('@pastash/pastash').base_output,
   util = require('util'),
   logger = require('@pastash/pastash').logger;
 
-global.Olm = require('olm');
-var sdk = require("matrix-js-sdk");
-
 function OutputMatrix() {
   base_output.BaseOutput.call(this);
   this.mergeConfig(this.serializer_config('raw'));
   this.mergeConfig({
     name: 'Matrix',
-    optional_params: ['path', 'userid', 'token', 'roomid', 'server', 'debug'],
+    optional_params: ['path', 'userid', 'token', 'roomid', 'server', 'olm', 'debug'],
     default_values: {
       'server': 'https://matrix.org',
       'userId': false,
       'roomId': false,
       'token': false,
+      'olm': false,
       'debug': false
     },    start_hook: this.start
   });
@@ -26,35 +24,42 @@ util.inherits(OutputMatrix, base_output.BaseOutput);
 OutputMatrix.prototype.start = function(callback) {
   logger.info('Start listening on matrix', this.roomId);
   if (!this.token||!this.userId||!this.roomId) { logger.info('Missing Settings!'); return; }
+
+  if (this.olm) global.Olm = require('olm');
+  var sdk = require("matrix-js-sdk");
+
   try {
         this.matrixClient = sdk.createClient({
             baseUrl: this.server,
             accessToken: this.token,
             userId: this.userId
         }.bind(this));
-        matrixClient.initCrypto();
+        if (this.olm) matrixClient.initCrypto();
         this.viewingRoom = false;
-        matrixClient.joinRoom(this.roomId).then(function(room) {
+        this.matrixClient.joinRoom(this.roomId).then(function(room) {
                     this.viewingRoom = room;
+		    logger.info("Connected to room", this.roomId, room.roomId);
                     callback();
-                }, function(err) {
+                }.bind(this), function(err) {
                     logger.error("Matrix Join Error: %s", err);
-                }.bind(this));
+                });
 
         this.matrixClient.on("sync", function(state, prevState, data) {
-            logger.info("Matrix Sync", state);
+            if (this.debug) logger.info("Matrix Sync", state);
         });
 
         this.matrixClient.on("Room", function(state) {
-            logger.info("Matrix Sync", state);
+            if (this.debug) logger.info("Matrix Sync", state);
         });
 
         this.matrixClient.on("Room.timeline", function(event, room, toStartOfTimeline) {
             if (toStartOfTimeline) {
                 return; // don't emit paginated results
             }
-	    logger.info("Received", event.event);
+	    if (this.debug) logger.info("Received", event.event);
         }.bind(this));
+
+	this.matrixClient.startClient(0);
 
   } catch(e) { logger.error(e); }
 
@@ -64,7 +69,7 @@ OutputMatrix.prototype.start = function(callback) {
 
 OutputMatrix.prototype.process = function(data) {
   if (this.viewingRoom) {
-    this.matrixClient.sendTextMessage(viewingRoom.roomId, data).finally(function() {
+    this.matrixClient.sendTextMessage(this.viewingRoom.roomId, data).finally(function() {
 	if (this.debug) logger.info("sent message", data);
     });
   }
