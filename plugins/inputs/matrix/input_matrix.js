c4a88fc2dbcddc6e36885767ffab80d323de4acb
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-03-08 18:10:51 +0100

    Fix Matrix input (revert)

--- plugins/inputs/matrix/input_matrix.js
+++ plugins/inputs/matrix/input_matrix.js
@@ -1,49 +1,73 @@
 var base_input = require('@pastash/pastash').base_input,
   util = require('util'),
-  mqtt = require('mqtt'),
   logger = require('@pastash/pastash').logger;
 
-function InputMQTT() {
+function InputMatrix() {
   base_input.BaseInput.call(this);
   this.mergeConfig(this.unserializer_config());
   this.mergeConfig({
-    name: 'MQTT',
-    host_field: 'address',
-    optional_params: ['topic'],
+    name: 'Matrix',
+    optional_params: ['userId', 'token', 'roomId', 'server', 'olm', 'debug'],
+    default_values: {
+      'server': 'https://matrix.org',
+      'userId': false,
+      'roomId': false,
+      'token': false,
+      'olm': false,
+      'debug': false
+    },
     start_hook: this.start,
   });
 }
 
-util.inherits(InputMQTT, base_input.BaseInput);
+util.inherits(InputMatrix, base_input.BaseInput);
 
-InputMQTT.prototype.start = function(callback) {
-  logger.info('Connecting to MQTT Server', this.address);
+InputMatrix.prototype.start = function(callback) {
+  logger.info('Start listening on matrix', this.roomId);
 
-  this.socket = mqtt.connect(this.address);
+  if (this.olm) global.Olm = require('olm');
+  var sdk = require("matrix-js-sdk");
 
-  this.socket.on('connect', function() {
-        logger.info('Connected to MQTT Server', this.address);
-        this.socket.subscribe(this.topic);
-        callback();
-  }.bind(this));
+  if (!this.token||!this.userId||!this.roomId) { logger.info('Missing Settings!'); return; }
+  try {
+	var matrixClient = sdk.createClient({
+	    baseUrl: this.server,
+	    accessToken: this.token,
+	    userId: this.userId
+	});
+	if (this.olm) this.matrixClient.initCrypto();
+	this.viewingRoom = false;
+	matrixClient.joinRoom(this.roomId).then(function(room) {
+                    this.viewingRoom = room;
+		    callback();
 
-  this.socket.on('message', function(topic, data) {
-    try {
-      var obj = JSON.parse(data.toString());
-      obj.topic = topic;
-      this.emit('data', obj);
-    } catch(e) {
-      this.emit('data', data.toString());
-    }
-  }.bind(this));
+                }.bind(this), function(err) {
+                    logger.error("Matrix Join Error: %s", err);
+		    return;
+                });
+
+	matrixClient.on("sync", function(state, prevState, data) {
+	        if (this.debug) logger.info("Matrix Sync", state);
+	}.bind(this));
+
+	matrixClient.on("Room", function(state) {
+	        if (this.debug) logger.info("Matrix Sync", state);
+	}.bind(this));
+
+	matrixClient.on("Room.timeline", function(event, room, toStartOfTimeline) {
+	        this.emit('data', { message: JSON.stringify(event.event), source: 'matrix' });
+	}.bind(this));
+
+	matrixClient.startClient(0);
+
+  } catch(e) { logger.error(e); }
 };
 
-InputMQTT.prototype.close = function(callback) {
-  logger.info('Closing input MQTT', this.address);
-  try { this.socket.end() } catch(e) {}
+InputMatrix.prototype.close = function(callback) {
+  logger.info('Closing Matrix input', this.path);
   callback();
 };
 
 exports.create = function() {
-  return new InputMQTT();
+  return new InputMatrix();
 };
