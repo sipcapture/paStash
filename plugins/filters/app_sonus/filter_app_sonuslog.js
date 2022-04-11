ac71417721248cdafd8c24ad04dffe8874070811
Nuno M Reis <nuno.reis@talkdesk.com>
2022-04-11 16:53:35 +0100

    also consider TLS transport

--- plugins/filters/app_sonus/filter_app_sonuslog.js
+++ plugins/filters/app_sonus/filter_app_sonuslog.js
@@ -52,7 +52,7 @@ logger.info('Initialized App Sonus SysLog to SIP/HEP parser');
 	 }
 	 */
 
-	 if (last.indexOf('2.0/TCP') !== -1){
+	 if (last.indexOf('2.0/TCP') !== -1 || last.indexOf('2.0/TLS') !== -1){
 		rcinfo.protocol = 6;
          }
 
