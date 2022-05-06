6f1ac977e9a6aad74682191aeb16ceb6ca82b542
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-05-06 17:06:50 +0200

    basic post spans

--- plugins/filters/app_janus/filter_app_janus_tracer.js
+++ plugins/filters/app_janus/filter_app_janus_tracer.js
@@ -8,6 +8,8 @@ var base_filter = require('@pastash/pastash').base_filter,
   logger = require('@pastash/pastash').logger;
 
 var recordCache = require("record-cache");
+var fetch = require('node-fetch');
+
 
 function nano_now(date){ return (date * 1000) + '000' }
 function just_now(){ return new Date().getTime() }
@@ -16,10 +18,12 @@ function FilterAppJanusTracer() {
   base_filter.BaseFilter.call(this);
   this.mergeConfig({
     name: 'AppJanusTracer',
-    optional_params: ['debug', 'cacheSize', 'cacheAge'],
+    optional_params: ['debug', 'cacheSize', 'cacheAge', 'endpoint'],
     default_values: {
       'cacheSize': 50000,
       'cacheAge':  60000,
+      'endpoint': 'http://localhost:3100/tempo/api/push',
+      'bypass': true,
       'debug': false
     },
     start_hook: this.start,
@@ -37,6 +41,13 @@ FilterAppJanusTracer.prototype.start = async function(callback) {
   });
   this.cache = cache;
 
+  var sessions = recordCache({
+    maxSize: this.cacheSize,
+    maxAge: this.cacheAge,
+    onStale: false
+  });
+  this.sessions = sessions;
+
   logger.info('Initialized App Janus Event Tracer');
   callback();
 };
@@ -48,22 +59,41 @@ FilterAppJanusTracer.prototype.process = function(data) {
    if (!line.session_id || !line.handle_id) return data;
 
    if (line.type == 1){
-        var fingerprint = event.session_id;
 	var event = { name: line.event.name, event: line.event.name, id: line.session_id }
 	if (line.event.name == "created"){
-		// start root trace
-		this.cache.add(fingerprint, just_now());
+		// start root trace, do not update
+		this.sessions.add(event.session_id, just_now());
 	} else if (line.event.name == "destroyed"){
 		// end root trace
-		this.cache.remove(fingerprint);
+		this.sessions.remove(event.session_id);
+	}
+
+   } else if (line.type == 2) {
+	if (!line.event.data) return;
+	var event = { name: line.event.name, event: line.event.name, id: line.session_id, handle: line.handle_id }
+	// session tracing + reset
+	var previous_ts = this.cache.get(event.session_id, 1)[0] || 0;
+	event.duration = just_now() - parseInt(previous_ts);
+	this.cache.add(event.session_id, just_now());
+
+	if(event.name == "attached") {
+		// session_id, handle_id, opaque_id
+	} else if (event.name == "detached") {
+		// session_id, handle_id, opaque_id
+		this.cache.add(event.handle_id, just_now());
 	}
 
    } else if (line.type == 64){
-	if (!line.event.data) return;
-	var event = { name: line.event.plugin, event: line.event.data.event, id: line.event.data.id }
+	if (!line.event.data) return data;
+	var event = { name: line.event.plugin, event: line.event.data.event, id: line.event.data.id, handle: line.handle_id }
+	// session tracing + reset
+	var previous_ts = this.cache.get(event.session_id, 1)[0] || 0;
+	event.duration = just_now() - parseInt(previous_ts);
+	this.cache.add(event.session_id, just_now());
 
 	if (event.event == "joined"){
 		// session_id, handle_id, opaque_id, event.data.id
+		// correlate: session_id --> event.data.id
 		this.cache.add(event.data.id, session_id);
 	} else if (event.event == "configured"){
 		// session_id, handle_id, opaque_id, event.data.id
@@ -71,75 +101,56 @@ FilterAppJanusTracer.prototype.process = function(data) {
 		// session_id, handle_id, opaque_id, event.data.id
 		this.cache.add(event.data.id, session_id);
 	} else if (event.event == "unpublished"){
-		// event.data.id
+		// correlate: event.data.id --> session_id
 		event.session_id = this.cache.get(fingerprint_event, 1)[0] || false;
+		line.session_id = event.session_id;
 	} else if (event.event == "leaving"){
-		// event.data.id
+		// correlate: event.data.id --> session_id
 		event.session_id = this.cache.get(fingerprint_event, 1)[0] || false;
+		line.session_id = event.session_id;
 		this.cache.delete(event.data.id)
 	}
 
-   	var fingerprint_event = line.event.handle_id;
-	var cache = this.cache.get(fingerprint_event, 1);
-	// find parent id
-	if (cache.length < 1) {
-		this.cache.add(fingerprint_event, just_now());
-	} else {
-		event.parentId = line.session_id
-	}
-	// get previous ts
-	event.duration = just_now() - parseInt(cache[0]);
 	event.timestamp = line.timestamp;
-	event.tags = { room: event.room, event: event.event, display: event.display }
-	var trace = tracegen(event)
-	this.emit('output', trace)
-	// update ts
-        this.cache.add(fingerprint, just_now());
-   } else if(line.type == 2) {
-	if (!line.event.data) return;
-	var event = { name: line.event.name }
-	if(event.name == "attached") {
-		// session_id, handle_id, opaque_id
-		this.cache.add(event.name, session_id);
-	} else if (event.name == "detached") {
-		// session_id, handle_id, opaque_id
-		event.session_id = this.cache.get(fingerprint_event, 1)[0] || false;
-		this.cache.delete(event.name)
-	}
+	var trace = tracegen(event, this.endpoint)
+	if (!this.bypass) this.emit('output', trace)
+
    }
-   return data;
+   if (this.bypass) this.emit('output', data);
+
 };
 
 exports.create = function() {
   return new FilterAppJanusTracer();
 };
 
-function tracegen(event){
-  if (event.root || !event.parentId){
+async function tracegen(event, endpoint){
+  try {
     var trace = [{
-	 "id": event.room,
-	 "traceId": event.id,
+	 "id": event.id,
+	 "traceId": event.traceId,
 	 "timestamp": nano_time(event.timestamp),
 	 "duration": event.duration,
-	 "name": event.event,
-	 "tags": event.tags,
+	 "name": event.name,
 	  "localEndpoint": {
-	    "serviceName": event.emitter
+	    "serviceName": event.service
 	  }
     }]
-    return trace;
-  } else {
-    var trace = [{
-	 "id": event.room,
-	 "traceId": event.id,
-	 "parentId": event.parentId,
-	 "timestamp": nano_time(event.timestamp),
-	 "duration": event.duration,
-	 "name": event.event,
-	  "localEndpoint": {
-	    "serviceName": event.emitter
-	  }
-    }]
-    return trace;
-  }
+    if (event.parentId){ trace[0].parentId }
+    if (event.tags){ trace[0].tags }
+
+    // send event to endpoint
+    if(endpoint){
+
+	const response = await fetch(endpoint, {
+	  method: 'POST',
+	  body: JSON.stringify(trace),
+	  headers: {'Content-Type': 'application/json'}
+	});
+	const data = await response.json();
+    } else {
+	return trace
+    }
+
+  } catch(e) { console.log(e); return; }
 }
