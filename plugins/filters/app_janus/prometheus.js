b148fb54b3b2f82f1bac3816150360649f8782b5
Jachen Duschletta <jcduschletta@gmail.com>
2022-09-08 17:30:06 -0500

    Stateless tracing (#147)
    * wip
    
    * fix error on traceId
    
    * null'ing to assist gc
    
    * tags correction on type
    
    * Add Prom Telemetry Client
    
    * Added RTT and Local Packet Loss Histogram
    
    * Up'd version

---
+++ plugins/filters/app_janus/prometheus.js
@@ -0,0 +1,27 @@
+const client = require('prom-client')
+const Registry = client.Registry
+const registry = new Registry()
+const { EventEmitter } = require('events')
+
+const emitter = new EventEmitter()
+
+setInterval(async () => {
+  const metrics = await Promise.all(registry.getMetricsAsArray().map(descr => descr.get()))
+  metrics.forEach(metric => {
+    const streams = metric.values.map(value => ({
+      stream: {
+        ...Object.fromEntries(Object.entries(value.labels).map(e => [e[0], e[1].toString()])),
+        __name__: value.metricName || metric.name
+      },
+      values: [[`${Date.now()}000000`, '', value.value]]
+    }))
+    emitter.emit('data', { streams: streams })
+  })
+  registry.resetMetrics()
+}, 15000)
+
+module.exports = {
+  client,
+  registry,
+  emitter
+}
