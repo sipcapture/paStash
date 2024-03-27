5983a6761abd652168b1f8fc9897288033310d5a
dletta <jcduschletta@gmail.com>
2024-03-27 15:25:03 -0500

    Adding SSL, CA, SASL params, Improvements for sending

--- plugins/outputs/kafkajs/output_kafkajs.js
+++ plugins/outputs/kafkajs/output_kafkajs.js
@@ -1,8 +1,9 @@
 var base_output = require('@pastash/pastash').base_output,
   util = require('util'),
-  logger = require('@pastash/pastash').logger;
+  logger = require('@pastash/pastash').logger,
+  fs = require('fs');
 
-const { Kafka } = require('kafkajs')
+const { Kafka, logLevel } = require('kafkajs')
 var Logger;
 
 function OutputKafka() {
@@ -10,13 +11,16 @@ function OutputKafka() {
   this.mergeConfig(this.serializer_config('json_logstash'));
   this.mergeConfig({
     name: 'Kafka',
-    optional_params: ['kafkaHost', 'topic', 'partition', 'threshold_down', 'debug', 'sasl_user', 'sasl_pass' ],
+    optional_params: ['kafkaHost', 'topic', 'ssl', 'ca', 'partition', 'threshold_down', 'debug', 'sasl_user', 'sasl_pass', 'sasl_mechanism' ],
     default_values: {
       debug: false,
       threshold_down: 10,
       topic: false,
       partition: false,
-      sasl_user: false
+      sasl_user: false,
+      sasl_mechanism: 'plain',
+      ssl: false,
+      ca: '',
     },
     start_hook: this.start,
   });
@@ -28,30 +32,38 @@ OutputKafka.prototype.start = async function(callback) {
 
   if(!this.kafkaHost) return;
 
+  if(this.debug) {
+   var logLevelDebug = logLevel.DEBUG
+  } else {
+   var logLevelDebug = logLevel.INFO
+  }
+
   var kconf = {
   	clientId: 'paStash',
-  	brokers: [this.kafkaHost]
+  	brokers: this.kafkaHost.split(','),
+        logLevel: logLevelDebug
   }
 
   if (this.sasl_user && this.sasl_pass){
       kconf.sasl = {
-	    mechanism: 'scram-sha-256',
+	    mechanism: this.sasl_mechanism,
 	    username: this.sasl_user,
 	    password: this.sasl_pass,
       }
   }
+  
+  if (this.ssl) {
+    kconf.ssl = true
+    if (this.ca) {
+      kconf.ssl = {
+       rejectUnauthorized: false,
+       ca: [ fs.readFileSync(this.ca) ]
+      }
+    }
+  }
   this.kafka = new Kafka(kconf);
   this.producer = this.kafka.producer()
   await this.producer.connect()
-
-  this.producer.on('error', (err) => {
-    logger.warning('Kafka Client Error:', err);
-    this.error_count++;
-  });
-  this.producer.on('ready', () => {
-    console.log('Kafka Client Ready!');
-    this.error_count = 0;
-  });
 
   this.on_alarm = false;
   this.error_count = 0;
@@ -76,11 +88,11 @@ OutputKafka.prototype.check = function() {
 
 OutputKafka.prototype.process = async function(data) {
 
-  var d = [{ topic: this.topic, messages: JSON.stringify(data) }];
+  var d = { topic: this.topic, messages: [{"key":"hepic-cdr","value":JSON.stringify(data)}] };
   try {
     if (this.partition){
       if (data.message) {
-	if (typeof data.message != 'object') data.message = JSON.parse(data.message);
+        if (typeof data.message != 'object') data.message = JSON.parse(data.message);
         d[0].key = data.message[this.partition];
       } else {
         d[0].key = data[this.partition];
@@ -92,6 +104,7 @@ OutputKafka.prototype.process = async function(data) {
 
   if (this.debug) logger.info("Preparing to send to Kafka\n", d[0], "\n\n" );
   try {
+    await this.producer.connect()
     await this.producer.send(d)
   } catch (e) {
     logger.error(e);
@@ -101,7 +114,7 @@ OutputKafka.prototype.process = async function(data) {
 
 OutputKafka.prototype.close = async function(callback) {
   logger.info('Closing Kafka Output to', this.kafkaHost);
-  await producer.disconnect()
+  await this.producer.disconnect()
   callback();
 };
 
--- plugins/outputs/kafkajs/readme.md
+++ plugins/outputs/kafkajs/readme.md
@@ -1,7 +1,7 @@
 KafkaJS output plugin
 ---
 
-Status : experimental plugin, unit tested and maintained.
+Status : plugin, unit tested and maintained.
 
 This plugin is used to send logs to Kafka.
 
@@ -21,6 +21,27 @@ Parameters :
 * ``kafkaHost``: Kafka host:port sets delimited by comma, _Example: 10.0.0.1:9092,10.0.0.2:9092_. Required. 
 * ``topic``: Kafka topic. Required.
 * ``partition``: Field to extract and use as Partition value for indexing. Optional.
+* ``ssl``: Set SSL to true to use tls transport. Optional.
+* ``ca``: Specify Path to customer certificate authority. Optional.
 * ``sasl_user``: SASL Username. Optional.
 * ``sasl_pass``: SASL Password. Optional.
+* ``sasl_mechanims``: SASL Mechanism. Optional. Default: 'plain' Options: 'scram-sha-256', 'scram-sha-512'
 * ``debug``: Debug option. Optional.
+
+Examples :
+
+Configuration using SSL and SASL to authenticate and connect.
+
+```
+output {
+  kafkajs {
+    topic => "your-topic-here"
+    kafkaHost => "receiver:9092"
+    debug => false
+    ssl => true
+    sasl_mechanism => "plain"
+    sasl_user => "pastash"
+    sasl_password => "supersecretpass"
+  }
+}
+```
